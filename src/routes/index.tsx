d3e992741a5484fee51e8ac32f66f7ea6132ad41
deveshdubey23 <devesh2302@gmail.com>
2026-07-02 01:04:43 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,8 +2,6 @@ import { createFileRoute } from "@tanstack/react-router";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useMemo, useState } from "react";
 import {
-  Sun,
-  Moon,
   Settings,
   Search,
   SlidersHorizontal,
